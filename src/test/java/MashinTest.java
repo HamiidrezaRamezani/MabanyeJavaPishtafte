22fa1409cf8b86a33461fe3b82f3cb83141444b3
Hamidreza <hamiidreza0121@gmail.com>
2025-11-04 08:49:43 +0330

    Objects creation

--- src/test/java/MashinTest.java
+++ src/test/java/MashinTest.java
@@ -1,16 +1,52 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MashinTest{
+
+
+    @Test
+    void mashin_bayad_harekat_konad_vaghti_mashin_roshan_ast_va_dar_baste_ast() {
+
+
+        Mashin mashin = new Mashin();
+
+
+
+
+        //Given
+        Mashin benz = new Mashin("Benz");
+        //When
+        benz.bastanDar();
+        benz.roshan();
+
+        boolean ayaBenzDarHarekatAst = benz.ayaMashinDarHarekatAst();
+        //Then
+
+        Assertions.assertFalse(benz.ayaDarMashinBazAst);
+        Assertions.assertTrue(ayaBenzDarHarekatAst);
+
+    }
 
     class Mashin{
 
 
+        Mashin(){
+            System.out.println("Constructor without any things");
+        }
+
+
+        Mashin(String mark){
+            this.mark = mark;
+            System.out.println("Yek mashin ba name: " + mark + " sakhte shod!");
+        }
+
         //Variables
         boolean ayaMashinRoshanAst;
         boolean ayaDarMashinBazAst;
-        double toul;
-        double arz;
         String mark;
 
         void roshan(){
+            ayaMashinRoshanAst = true;
             System.out.println("Mashin roshan ast");
         }
 
@@ -21,7 +57,7 @@ public class MashinTest{
 
         void bazKardanDar(){
             System.out.println("Dar baz shod");
-            ayaMashinRoshanAst = true;
+            ayaDarMashinBazAst = true;
         }
 
         void bastanDar(){
@@ -30,11 +66,13 @@ public class MashinTest{
         }
 
 
-        void harekat(){
+        boolean ayaMashinDarHarekatAst(){
             if(!ayaDarMashinBazAst && ayaMashinRoshanAst){
                 System.out.println("Mashin dar hal harekat ast");
+                return true;
             }else{
                 System.out.println("Mashin park ast");
+                return false;
             }
         }
     }
